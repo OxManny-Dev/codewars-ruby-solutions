37201585a7d474bef7a0f1cb1b02e8ebd2e2ba96
emmanuel <emmanueljucaban@gmail.com>
2016-05-30 07:40:37 +0000

    Added new problem

---
+++ even or odd.rb
@@ -0,0 +1,7 @@
+# Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+
+
+def even_or_odd(number)
+   number.even? ? "Even" : "Odd"
+end
