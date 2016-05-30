3a0491f435e2d09789f0c2cc10d688f30ded3592
emmanuel <emmanueljucaban@gmail.com>
2016-05-30 07:53:38 +0000

    Added new problem

---
+++ broken problems.rb
@@ -0,0 +1,25 @@
+# Correct this code, so that the greet function returns the expected value.
+
+# class Person
+#   def initialize(name)
+#     @name = name
+#   end
+  
+#   def greet(other_name)
+#     "Hi #{other_name}, my name is #{name}"
+#   end
+# end
+
+
+# Solution
+
+class Person
+  attr_accessor :name
+  def initialize(name)
+    @name = name
+  end
+  
+  def greet(other_name)
+    "Hi #{other_name}, my name is #{name}"
+  end
+end
