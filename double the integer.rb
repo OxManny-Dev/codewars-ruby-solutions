ad4ed78eee8e28f353a8223a1f2808ba5ce66d4a
emmanuel <emmanueljucaban@gmail.com>
2016-05-30 07:42:41 +0000

    Added new problem

---
+++ double the integer.rb
@@ -0,0 +1,12 @@
+# Code as fast as you can! You need to double the integer and return it.
+
+# def doubleInteger(i)
+#   # Double the input!
+# end
+
+
+def doubleInteger(i)
+  # Double the input!
+  i * 2
+end
+
