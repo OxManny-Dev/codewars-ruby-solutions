8e75ea142b34bd239a150d44518da41b40287174
emmanuel <emmanueljucaban@gmail.com>
2016-06-01 08:46:18 +0000

    Solved basic variable assignment

---
+++ basic variable assignment.rb
@@ -0,0 +1,12 @@
+# This code should store "codewa.rs" as a variable called "name" but it's not working. Can you figure out why?
+
+
+# a == "code"
+# b == "wa.rs"
+# name == a + b
+
+# Solution
+
+a = "code"
+b = "wa.rs"
+name = a + b
