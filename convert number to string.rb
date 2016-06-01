13668b87b06296648a0cbe56ba4e6e8bd880ab07
emmanuel <emmanueljucaban@gmail.com>
2016-06-01 08:43:56 +0000

    Solved convert number to string

---
+++ convert number to string.rb
@@ -0,0 +1,14 @@
+# We need a function that can transform a number into a string.
+
+# What ways of achieving this do you know?
+
+
+# def numberToString(num)
+#   # Convert a Number to a String
+# end
+
+
+def numberToString(num)
+  # Convert a Number to a String
+  num.to_s
+end
