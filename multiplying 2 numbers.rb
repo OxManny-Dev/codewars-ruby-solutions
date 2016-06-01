03d8e892605b73195c9f74e1e59c14196a720731
emmanuel <emmanueljucaban@gmail.com>
2016-06-01 09:35:34 +0000

    Solved push a hash or object into array

---
+++ multiplying 2 numbers.rb
@@ -0,0 +1,10 @@
+# This function has to be called multiply and needs to take two numbers as arguments,
+
+#  and has to return the multiplication of the two arguments.
+
+
+# Write here your "multiply"-function
+
+def multiply (a ,b)
+  a * b
+end
