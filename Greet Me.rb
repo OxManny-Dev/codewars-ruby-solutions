3a1c89c7831da8c7feecc8cc6f6134045ff5b065
emmanuel <emmanueljucaban@gmail.com>
2016-06-01 10:41:09 +0000

    Solved greet me

---
+++ Greet Me.rb
@@ -0,0 +1,12 @@
+# Write a method that takes one argument as name and then greets that name, capitalized and ends with an exclamation point.
+
+# Example:
+
+# greet("maya") # returns "Hello Maya!"
+# greet("JACK") # returns "Hello Jack!"
+
+
+#  Solution
+def greet(name)
+  "Hello #{name.capitalize}!"
+end
