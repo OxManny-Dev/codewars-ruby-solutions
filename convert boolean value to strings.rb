a152cf4abacb704331247d518a0348a46268b7da
emmanuel <emmanueljucaban@gmail.com>
2016-06-01 08:31:16 +0000

    Solved convert boolean value to strings

---
+++ convert boolean value to strings.rb
@@ -0,0 +1,18 @@
+# Complete the bool_to_word (Javascript: boolToWord ) method.
+
+# Given: a boolean value
+
+# Return: a 'Yes' string for true and a 'No' string for false
+
+
+# def bool_to_word bool
+#   # TODO
+# end
+
+
+
+
+def bool_to_word bool
+  # TODO
+  bool ? "Yes" : "No"
+end
