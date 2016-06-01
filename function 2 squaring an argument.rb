5ed1ba35337f4860078ab94d624c945a039de7a6
emmanuel <emmanueljucaban@gmail.com>
2016-06-01 09:29:41 +0000

    Solved function 2 squaring an argument

---
+++ function 2 squaring an argument.rb
@@ -0,0 +1,7 @@
+# Now you have to write a function called square that takes an argument and returns the square of it.
+
+# Write the "square"-function here
+
+def square number
+  number * number
+end
